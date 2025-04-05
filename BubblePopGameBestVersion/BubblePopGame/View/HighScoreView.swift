fecfc0c0200aab2093fee49738d7faf361aadedc
JanH10111 <janrobin.huecking@student.uts.edu.au>
2025-04-05 16:21:59 +1100

    Add files via upload

---
+++ BubblePopGameBestVersion/BubblePopGame/View/HighScoreView.swift
@@ -0,0 +1,18 @@
+//
+//  HighScoreView.swift
+//  BubblePopGame
+//
+//  Created by Jan Huecking on 1/4/2025.
+//
+
+import SwiftUI
+
+struct HighScoreView: View {
+    var body: some View {
+        Text("High Score")
+    }
+}
+
+#Preview {
+    HighScoreView()
+}
